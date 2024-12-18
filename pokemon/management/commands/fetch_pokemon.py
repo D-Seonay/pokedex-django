5b4c373c7b6ebb8d7bdb670903d29bfd165d3614
Mathéo DELAUNAY <dseonay@gmail.com>
2024-12-18 10:56:16 +0100

    Ajout de nouveaux attributs au modèle Pokémon : nombre, statistiques de combat et URL du cri. Mise à jour des templates pour afficher ces informations.

--- pokemon/management/commands/fetch_pokemon.py
+++ pokemon/management/commands/fetch_pokemon.py
@@ -6,19 +6,31 @@ class Command(BaseCommand):
     help = "Fetch Pokémon data from PokeAPI"
 
     def handle(self, *args, **kwargs):
-        api_url = "https://pokeapi.co/api/v2/pokemon?limit=151"  # Récupère les 50 premiers Pokémon
+        api_url = "https://pokeapi.co/api/v2/pokemon?limit=151"  # Récupère les 10 premiers Pokémon
         response = requests.get(api_url)
         data = response.json()
         
         for pokemon_data in data['results']:
             details = requests.get(pokemon_data['url']).json()
+            
+            # Génération de l'URL du cri
+            cry_url = f"https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/{details['id']}.ogg"
+            
             Pokemon.objects.update_or_create(
                 name=details['name'],
                 defaults={
                     'sprite': details['sprites']['front_default'],
+                    "number": details['id'],
                     'types': ", ".join(t['type']['name'] for t in details['types']),
                     'height': details['height'],
-                    'weight': details['weight']
+                    'weight': details['weight'],
+                    'hp': details['stats'][0]['base_stat'],
+                    'attack': details['stats'][1]['base_stat'],
+                    'defense': details['stats'][2]['base_stat'],
+                    'special_attack': details['stats'][3]['base_stat'],
+                    'special_defense': details['stats'][4]['base_stat'],
+                    'speed': details['stats'][5]['base_stat'],
+                    'cry_url': cry_url,
                 }
             )
         self.stdout.write(self.style.SUCCESS("Pokémon data fetched successfully!"))
